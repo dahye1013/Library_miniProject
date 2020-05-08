0fe938c795d815be38e25f98ab571553256c7d09
dahye1013 <64780560+dahye1013@users.noreply.github.com>
2020-05-08 16:48:08 +0900

    Add files via upload

---
+++ ManagerFrame/BasicFrameMember.java
@@ -0,0 +1,52 @@
+package ManagerFrame;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTabbedPane;
+
+public class BasicFrameMember extends JFrame {
+    public BookManagement p1;
+    public LendBookManage p2;
+    public Panel03 p3;
+    
+    public BasicFrameMember() {
+        super("Library");
+
+        
+        JTabbedPane t = new JTabbedPane(); // JTabbedPane생성
+
+        p1 = new BookManagement();
+        p2 = new LendBookManage();
+        p3 = new Panel03();
+         
+        JTabbedPane tab = new JTabbedPane();
+        tab.add(p1,"BookManage");
+        tab.add(p2,"LendManage");
+        tab.add(p3,"p3");
+        
+        add(tab);
+        
+        setBounds(700, 100, 1100, 800);
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter(){
+            @Override
+            public void windowClosing(WindowEvent e){
+                int result=  JOptionPane.showConfirmDialog(null, "종료하시겠습니까?");
+                if(result == JOptionPane.NO_OPTION)//저장하지않고 종료
+                  return;
+            }
+        });
+            
+    }
+
+    public static void main(String[] args) {
+        new BasicFrameMember();
+    }
+
+}
+
