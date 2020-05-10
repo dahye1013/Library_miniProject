3be3de35308b1a30625c79c7673ebede997ef0cc
dahye1013 <64780560+dahye1013@users.noreply.github.com>
2020-05-10 14:59:08 +0900

    Add files via upload

--- Login/FindID.java
+++ Login/FindID.java
@@ -1,61 +1,80 @@
 package Login;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class FindID extends JFrame implements ActionListener {
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	JFrame frame;
+	JTextField nickNameT;
+	JLabel nickNameL;
+	JLabel emailL;
+	JTextField emailT;
 	JButton findB, cancelB;
-	JLabel lblNewLabel = new JLabel("");
+	JLabel title = new JLabel("");
+
+	public void paintComponent(Graphics g) {
+		Dimension d = getSize();
+		ImageIcon img = new ImageIcon("Images/login.jpg");
+		g.drawImage(img.getImage(), 0, 0, d.width, d.height, null);
+	}
 
 	FindID() {
-		lblNewLabel.setText("Find ID");
+
+		title.setText("ID찾기");
 		setBounds(700, 200, 300, 250);
-		getContentPane().setLayout(null);
 		setVisible(true);
 		setResizable(false);
+		setBackground(Color.white);
 
-		lblNewLabel.setFont(new Font("굴림", Font.PLAIN, 19));
-		lblNewLabel.setBounds(105, 10, 64, 21);
-		getContentPane().add(lblNewLabel);
+		this.setLayout(null);
 
-		JLabel lblNewLabel_1 = new JLabel("이름");
-		lblNewLabel_1.setFont(new Font("굴림", Font.PLAIN, 14));
-		lblNewLabel_1.setBounds(48, 62, 57, 15);
-		getContentPane().add(lblNewLabel_1);
+		title.setFont(new Font("굴림", Font.PLAIN, 19));
+		title.setBounds(105, 10, 64, 21);
+		this.add(title);
 
-		textField = new JTextField();
-		textField.setBounds(127, 60, 116, 21);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		nickNameL = new JLabel("닉네임");
+		nickNameL.setFont(new Font("굴림", Font.PLAIN, 14));
+		nickNameL.setBounds(48, 62, 57, 15);
+		this.add(nickNameL);
 
-		JLabel lblEmail = new JLabel("이메일");
-		lblEmail.setFont(new Font("굴림", Font.PLAIN, 14));
-		lblEmail.setBounds(48, 98, 57, 15);
-		getContentPane().add(lblEmail);
+		nickNameT = new JTextField();
+		nickNameT.setBounds(127, 60, 116, 21);
+		getContentPane().add(nickNameT);
+		nickNameT.setColumns(10);
 
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(127, 96, 116, 21);
-		getContentPane().add(textField_1);
+		emailL = new JLabel("이메일");
+		emailL.setFont(new Font("굴림", Font.PLAIN, 14));
+		emailL.setBounds(48, 98, 57, 15);
+		this.add(emailL);
+
+		emailT = new JTextField();
+		emailT.setColumns(10);
+		emailT.setBounds(127, 96, 116, 21);
+		this.add(emailT);
 
 		findB = new JButton("찾기");
 		findB.setBounds(37, 140, 97, 39);
-		getContentPane().add(findB);
+		findB.setBackground(Color.white);
+		this.add(findB);
 
 		cancelB = new JButton("취소");
 		cancelB.setBounds(146, 140, 97, 39);
-		getContentPane().add(cancelB);
+		cancelB.setBackground(Color.white);
+		this.add(cancelB);
 
 		findB.addActionListener(this);
 		cancelB.addActionListener(this);
@@ -72,11 +91,26 @@ public class FindID extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == findB) {
-
+			if (SignUpManager.list.size() != 0) {
+				for (int i = 0; i < SignUpManager.list.size(); i++) {
+					if (nickNameT.getText().equals(SignUpManager.list.get(i).getId())) {
+						JOptionPane.showMessageDialog(this,
+								"회원님의 아이디는 [" + SignUpManager.list.get(i).getId() + "] 입니다");
+					}
+				}
+			} else if (SignUpMember.list.size() != 0) {
+				for (int i = 0; i < SignUpMember.list.size(); i++) {
+					if (nickNameT.getText().equals(SignUpMember.list.get(i).getId())) {
+						JOptionPane.showMessageDialog(this, "회원님의 아이디는 [" + SignUpMember.list.get(i).getId() + "] 입니다");
+					}
+				}
+			} else {
+				JOptionPane.showMessageDialog(this, "등록된 회원정보가 없습니다.");
+			}
 		} else if (e.getSource() == cancelB) {
 			dispose();
 		}
 
 	}
-	
+
 }
--- Login/FindPW.java
+++ Login/FindPW.java
@@ -9,56 +9,57 @@ import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class FindPW extends JFrame implements ActionListener{
     private JFrame frame;
-    private JTextField textField;
-    private JTextField textField_1;
+    private JTextField idT;
+    private JTextField emailT;
     JButton findB, cancelB;
-    JLabel lblNewLabel = new JLabel("");
+    JLabel title = new JLabel("");
     
     FindPW(){
-        lblNewLabel.setText("Find PW");
+        title.setText("PW찾기");
         setBounds(700, 200, 300, 250);
         getContentPane().setLayout(null);
         setVisible(true);
         setResizable(false);
         
-        lblNewLabel.setFont(new Font("굴림", Font.PLAIN, 19));
-        lblNewLabel.setBounds(105, 10, 64, 21);
-        getContentPane().add(lblNewLabel);
+        title.setFont(new Font("굴림", Font.PLAIN, 19));
+        title.setBounds(105, 10, 64, 21);
+        getContentPane().add(title);
 
-        JLabel lblNewLabel_1 = new JLabel("이름");
+        JLabel lblNewLabel_1 = new JLabel("ID");
         lblNewLabel_1.setFont(new Font("굴림", Font.PLAIN, 14));
         lblNewLabel_1.setBounds(48, 62, 57, 15);
         getContentPane().add(lblNewLabel_1);
 
-        textField = new JTextField();
-        textField.setBounds(127, 60, 116, 21);
-        getContentPane().add(textField);
-        textField.setColumns(10);
+        idT = new JTextField();
+        idT.setBounds(127, 60, 116, 21);
+        getContentPane().add(idT);
+        idT.setColumns(10);
 
         JLabel lblEmail = new JLabel("이메일");
         lblEmail.setFont(new Font("굴림", Font.PLAIN, 14));
         lblEmail.setBounds(48, 98, 57, 15);
         getContentPane().add(lblEmail);
 
-        textField_1 = new JTextField();
-        textField_1.setColumns(10);
-        textField_1.setBounds(127, 96, 116, 21);
-        getContentPane().add(textField_1);
+        emailT = new JTextField();
+        emailT.setColumns(10);
+        emailT.setBounds(127, 96, 116, 21);
+        getContentPane().add(emailT);
 
-        JButton findB = new JButton("찾기");
+        findB = new JButton("찾기");
         findB.setBounds(37, 140, 97, 39);
         getContentPane().add(findB);
 
-        JButton canelB = new JButton("취소");
-        canelB.setBounds(146, 140, 97, 39);
-        getContentPane().add(canelB);
+        cancelB = new JButton("취소");
+        cancelB.setBounds(146, 140, 97, 39);
+        getContentPane().add(cancelB);
         
         findB.addActionListener(this);
-        canelB.addActionListener(this);
+        cancelB.addActionListener(this);
         
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -72,7 +73,22 @@ public class FindPW extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==findB) {
-            
+        	if (SignUpManager.list.size() != 0) {
+				for (int i = 0; i < SignUpManager.list.size(); i++) {
+					if (idT.getText().equals(SignUpManager.list.get(i).getId())) {
+						JOptionPane.showMessageDialog(this,
+								"회원님의 비밀번호는 [" + SignUpManager.list.get(i).getPassword() + "] 입니다");
+					}
+				}
+			} else if (SignUpMember.list.size() != 0) {
+				for (int i = 0; i < SignUpMember.list.size(); i++) {
+					if (idT.getText().equals(SignUpMember.list.get(i).getId())) {
+						JOptionPane.showMessageDialog(this, "회원님의 비밀번호는 [" + SignUpMember.list.get(i).getPassword() + "] 입니다");
+					}
+				}
+			} else {
+				JOptionPane.showMessageDialog(this, "등록된 회원정보가 없습니다.");
+			}
         }else if(e.getSource()==cancelB) {
         	dispose();
         }
